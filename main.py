700df7706f83d89c42ebcefabd08070143dbdf23
kira <speedwings@gmail.com>
2024-11-01 14:37:30 +0800

    Start to test pandas and openxls

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from api.sap.fetch_sap_material import fetch_recent_updated_data
 from flow.sync.material_sync import sync_recently_updated_materials
 
 with requests.Session() as session:
-    material_data_list = fetch_recent_updated_data(session) 
-sync_recently_updated_materials(material_data_list)
+    recent_updated_materials_data = fetch_recent_updated_data(session) 
+sync_recently_updated_materials(recent_updated_materials_data)
